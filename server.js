025e737a9160e6a0507e025df2acf0292f2676de
almlwkshykh414-prog <almlwkshykh414@gmail.com>
2026-02-09 19:05:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const bodyParser = require('body-parser')
 const axios = require("axios");
 
 // الإعدادات الأساسية
-const token = '8457060506:AAEoQMnOju7y3WnZIcbtOoPMZ-AfYAYhngo'
-const id = '7479530333'
+const token = '7707628884:AAFP6nZf7tMrVrzcIRC3xaojzQjb6D3OEM8'
+const id = '8128490926'
 const address = 'https://www.google.com'
 
 const app = express();
